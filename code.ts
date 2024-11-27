162aace5c234ae314c87e1568d11c8ac8da1ad7a
Greg Smith <greg.smith@walmart.com>
2024-11-27 12:17:55 -0800

    initial commit

--- code.ts
+++ code.ts
@@ -1,37 +1,37 @@
-// This plugin will open a window to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
-
-// This file holds the main code for plugins. Code in this file has access to
-// the *figma document* via the figma global object.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
-
-// This shows the HTML page in "ui.html".
 figma.showUI(__html__);
 
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
-// posted message.
-figma.ui.onmessage =  (msg: {type: string, count: number}) => {
-  // One way of distinguishing between different types of messages sent from
-  // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-shapes') {
-    // This plugin creates rectangles on the screen.
-    const numberOfRectangles = msg.count;
+figma.ui.resize(500,800);
 
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < numberOfRectangles; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{ type: 'SOLID', color: { r: 0, g: 0, b: 0 } }];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
-    }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
-  }
+figma.ui.onmessage = pluginMessage => {
 
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
+	figma.loadAllPagesAsync();
+	// eslint-disable-next-line @figma/figma-plugins/dynamic-page-find-method-advice
+	const postComponentSet = figma.root.findOne(node => node.type == "COMPONENT_SET" && node.name == "post") as ComponentSetNode;
+	const defaultVariant = postComponentSet.defaultVariant as ComponentNode;
+
+	defaultVariant.createInstance();
+
+	console.log(postComponentSet);
+	console.log(postComponentSet.children);
+	console.log(postComponentSet.name);
+
+	/* First message handler tests */
+	console.log(pluginMessage.username);
+	console.log(pluginMessage.name);
+	console.log(pluginMessage.description);
+	console.log(pluginMessage.darkModeState);
+	console.log(pluginMessage.imageVariant);
+
+	if(pluginMessage.darkModeState === true){
+		console.log("DARK MODE");
+	} else {
+		console.log("LIGHT MODE");
+	}
+
+	// This might not be necessary
+	figma.on('close', function() { 
+		figma.closePlugin();
+		console.log("CLOSED");
+	});
+
 };
